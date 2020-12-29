c242fceb06aac6b01883324c5a45a094d71d37df
Vatsal <rdbcasillas11@gmail.com>
2020-12-29 17:37:01 -0600

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="@/assets/images/branding.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
+        />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item to="/about" tag="router-link">
+        About
+      </b-navbar-item>
+    </template>
+
+    <template slot="end"> </template>
+  </b-navbar> -->
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="/">
+        Home
+      </a>
+      <a class="navbar-item" href="/about">
+        About
+      </a>
+    </div>
+  </div>
+</nav>
+</template>
